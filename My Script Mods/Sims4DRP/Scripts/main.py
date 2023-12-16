3a88981cd4431f5d33f1a1b9276e30bc62251386
Alexus <77337386+Otakubuns@users.noreply.github.com>
2023-12-16 17:52:31 -0500

    Fixed build/buy to previous version & added logging

--- My Script Mods/Sims4DRP/Scripts/main.py
+++ My Script Mods/Sims4DRP/Scripts/main.py
@@ -8,25 +8,29 @@ import services
 import sims4.reload
 from game_services import GameServiceManager, service_manager
 from sims.funds import FamilyFunds
+from sims.household_utilities.utilities_manager import ZoneUtilitiesManager
 from sims4 import commands
 from sims4.service_manager import Service
 from world import lot
 
 # Sims 4 Discord Rich Presence
 # Created by: Otakubuns
-# Version: 1.0.3
-# Last Updated: 2023-05-25
+# Version: 1.0.4
+# Last Updated: 2023-12-16
 # Description: Adds Discord Rich Presence to The Sims 4 with Injection methods for CAS, Build/Buy, and Live Mode.
-#              Also adds world icons and household funds & name.
+#              Also adds world icons, household funds & name.
 
 # DRP Variables
 client_id = '971558123531804742'
 client = rpc.DiscordIpcClient.for_platform(client_id)
 start_time = mktime(time.localtime())
 
-# Start presence right as the game starts
-client.set_activity(details="Browsing the menu", large_text="Main Menu", large_image="menu", start=start_time)
+# Global service variable
+with sims4.reload.protected(globals()):
+    _my_custom_service = None
 
+def get_my_custom_service():
+    return service_manager.my_custom_service
 
 def inject(target_function, new_function):
     @wraps(target_function)
@@ -45,19 +49,9 @@ def inject_to(target_object, target_function_name):
     return _inject_to
 
 
-# Global service variable
-with sims4.reload.protected(globals()):
-    _my_custom_service = None
-
-
-def get_my_custom_service():
-    return service_manager.my_custom_service
-
-
 # Storage for variables for easier updating
 gamemode_image = None
 gamemode_text = None
-current_zone_id = None
 
 
 class MyCustomService(Service):
@@ -68,25 +62,70 @@ class MyCustomService(Service):
     def on_zone_load(self, *args, **kwargs):
         global gamemode_text
         global gamemode_image
-        global current_zone_id
         gamemode_text = "Live Mode"
         gamemode_image = "live"
-        current_zone_id = services.current_zone_id()
         client.set_activity(
             details=GetWorldName(),
             state=f"{GetHouseholdName()} | §{GetHouseholdFunds()}",
             large_image=GetWorldKey(),
-            large_text=GetLotName(),
+            large_text=GetWorldName(),
             small_image=gamemode_image,
             small_text=gamemode_text,
             start=start_time)
 
     def start(self, *args, **kwargs):
-        pass
+        build_buy.register_build_buy_enter_callback(self.on_build_buy_enter)
+        build_buy.register_build_buy_exit_callback(self.on_build_buy_exit)
 
     def stop(self, *args, **kwargs):
         global _my_custom_service
+        build_buy.unregister_build_buy_enter_callback(self.on_build_buy_enter)
+        build_buy.unregister_build_buy_exit_callback(self.on_build_buy_exit)
         _my_custom_service = None
+
+    def on_build_buy_enter(self, *args, **kwargs):
+        global gamemode_text
+        global gamemode_image
+        gamemode_text = "Build/Buy Mode"
+        gamemode_image = "buildbuy"
+
+        if services.active_household() is None:
+            client.set_activity(details=GetWorldName(),
+                                state="Editing A Lot",
+                                large_image=GetWorldKey(),
+                                large_text=GetLotName(),
+                                small_image=gamemode_image,
+                                small_text=gamemode_text,
+                                start=start_time)
+        else:
+            client.set_activity(
+                details=GetWorldName(),
+                state=f"{GetHouseholdName()} | §{GetHouseholdFunds()}",
+                large_image=GetWorldKey(),
+                large_text=GetWorldName(),
+                small_image=gamemode_image,
+                small_text=gamemode_text,
+                start=start_time)
+
+    def on_build_buy_exit(self, *args, **kwargs):
+        global gamemode_text
+        global gamemode_image
+        gamemode_text = "Live Mode"
+        gamemode_image = "live"
+
+        if services.active_household() is None:
+            # If no household is active, we're in manage worlds
+            client.set_activity(details="In Manage Worlds", large_text="Manage Worlds", large_image="menu",
+                                start=start_time)
+        else:
+            client.set_activity(
+                details=GetWorldName(),
+                state=f"{GetHouseholdName()} | §{GetHouseholdFunds()}",
+                large_image=GetWorldKey(),
+                large_text=GetWorldName(),
+                small_image=gamemode_image,
+                small_text=gamemode_text,
+                start=start_time)
 
 
 @inject_to(GameServiceManager, 'start_services')
@@ -96,9 +135,9 @@ def start_services(original, self, *args, **kwargs):
         if _my_custom_service is None:
             _my_custom_service = MyCustomService()
             self.register_service(_my_custom_service, is_init_critical=False)
-
     except:
         pass
+
     original(self, *args, **kwargs)
 
 
@@ -106,66 +145,6 @@ def start_services(original, self, *args, **kwargs):
 def inject_main_menu_load(original):
     client.set_activity(details="Browsing the menu", large_text="Main Menu", large_image="menu", start=start_time)
     original()
-
-
-@inject_to(build_buy, 'c_api_buildbuy_session_begin')
-def inject_build_buy_enter(original, zone_id, account_id):
-    original(zone_id, account_id)
-    global current_zone_id
-    global gamemode_text
-    global gamemode_image
-    gamemode_text = "Build/Buy Mode"
-    gamemode_image = "buildbuy"
-
-    # If the zone id is different it's in manage world build/buy so update the presence
-    try:
-        if current_zone_id != zone_id:
-            client.set_activity(details=GetWorldName(),
-                                state=f"Editing A Lot",
-                                large_image=GetWorldKey(),
-                                large_text=GetLotName(),
-                                small_image=gamemode_image,
-                                small_text=gamemode_text,
-                                start=start_time)
-
-        else:
-            client.set_activity(
-                details=GetWorldName(),
-                state=f"{GetHouseholdName()} | §{GetHouseholdFunds()}",
-                large_image=GetWorldKey(),
-                large_text=GetLotName(),
-                small_image=gamemode_image,
-                small_text=gamemode_text,
-                start=start_time)
-    except Exception:
-        pass
-
-
-@inject_to(build_buy, 'c_api_buildbuy_session_end')
-def inject_build_buy_exit(original, zone_id, account_id, **kwargs):
-    global current_zone_id
-    global gamemode_text
-    global gamemode_image
-    gamemode_text = "Live Mode"
-    gamemode_image = "live"
-
-    try:
-        # If the zone id is different it's in manage world build/buy so update the presence
-        if current_zone_id != zone_id:
-            client.set_activity(details="In Manage Worlds", large_text="Manage Worlds", large_image="menu",
-                                start=start_time)
-        else:
-            client.set_activity(
-                details=GetWorldName(),
-                state=f"{GetHouseholdName()} | §{GetHouseholdFunds()}",
-                large_image=GetWorldKey(),
-                large_text=GetLotName(),
-                small_image=gamemode_image,
-                small_text=gamemode_text,
-                start=start_time)
-    except Exception:
-        pass
-    original(zone_id, account_id, **kwargs)
 
 
 @inject_to(FamilyFunds, 'send_money_update')
